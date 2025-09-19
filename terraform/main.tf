a141fa3547bbbe203cc29dbf1696748e44c751ec
maxshen01 <maxshen2503@gmail.com>
2025-09-19 12:55:12 +0100

    make terraform files to provision ec2 instance in north virginia

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_instance" "backend_servers" {
+  ami = "ami-08982f1c5bf93d976"
+  key_name = "default-ec2"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.backend_servers_sg.id]
+
+  count = 1
+  tags = {
+    Name = "Backend_Server_${count.index + 1}"
+    environment = "dev"
+  }
+}
+
+resource "local_file" "frontend_env" {
+  filename = "${path.module}/../client/js/config/config.js"
+  content  = <<EOT
+export const API_URL = "http://${aws_instance.backend_servers[0].public_ip}"
+EOT
+}
