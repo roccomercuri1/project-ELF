8bac81f8357a1e03621f88cd9fb363535cf06a8d
IMuhim <muhim.istiak@gmail.com>
2025-09-23 14:53:33 +0100

    Admins can't review themselves

--- client/static/js/dataPage.js
+++ client/static/js/dataPage.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // const backBtn = document.getElementById("backBtn");
-  // if (backBtn) {
-  //   backBtn.addEventListener("click", () => {
-  //     window.location.href = "http://127.0.0.1:5500/project-ELF/client/pages/homepage.html";
-  //   });
-  // }
+  const homeBtn = document.getElementById("homeBtn");
+  if (homeBtn) {
+    homeBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.location.href =
+        "http://127.0.0.1:5500/client/pages/homepage.html";
+    });
+  }
 
   const logoutBtn = document.getElementById("logoutBtn");
   if (logoutBtn) {
     logoutBtn.addEventListener("click", (e) => {
       e.preventDefault();
       localStorage.clear();
-      window.location.assign("http://127.0.0.1:5500/project-ELF/client/pages/login.html");
+      window.location.assign(
+        "http://127.0.0.1:5500/project-ELF/client/pages/login.html"
+      );
     });
   }
 });
